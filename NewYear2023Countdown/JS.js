8e0002d84cdc2bc24bd3d4f5353143d0e03310dd
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-12-28 13:26:20 +0000

    Create JS.js

---
+++ NewYear2023Countdown/JS.js
@@ -0,0 +1,10 @@
+var countDownDate = new Date("Jan 1, 2023 00:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24)); 
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));  
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));  
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);                
+    document.getElementById("days").innerHTML = days;                          document.getElementById("hrs").innerHTML = hours;                             document.getElementById("mins").innerHTML = minutes;                              document.getElementById("secs").innerHTML = seconds;
+}, 1000);
