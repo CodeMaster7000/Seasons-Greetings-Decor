b3753a326e6ccb85975c7961912dfb68fea9265e
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-12-31 12:07:52 +0000

    Delete Christmas2023Countdown.py

--- Christmas2023Countdown.py
+++
@@ -1,6 +0,0 @@
-import datetime
-dt = datetime.datetime
-now = dt.now()
-timeLeft = dt(year = 2023, month = 12, day = 25) - dt(year=now.year, month=now.month, day=now.day)
-print "Days left until Christmas: "
-print timeLeft.days
